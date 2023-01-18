e8d0ccacafe78ba9ba92d468f02a79e7d9546c0b
Alva Suen <ymsym98@gmail.com>
2023-01-18 12:22:30 +0800

    modify main.ts

--- main.ts
+++ main.ts
@@ -19,7 +19,7 @@ export const client = new Client({
     password: process.env.DB_PASSWORD,
   });
 
-  client.connect();
+client.connect();
 
 //Socket.io
 const server = new http.Server(app);
@@ -30,17 +30,127 @@ const io = new SocketIO(server);
 const uploadDir = "uploads";
 fs.mkdirSync(uploadDir, { recursive: true });
 
+const form = formidable({
+  uploadDir,
+  keepExtensions: true,
+  maxFiles: 1,
+  maxFileSize: 200 * 1024 ** 2, // the default limit is 200KB
+  filter: (part) => part.mimetype?.startsWith("image/") || false,
+});
+
+function formidable_promise(req:express.Request){
+  return new Promise((resolve,reject)=>{
+    form.parse(req, (err, fields, files) => {
+      if({err}.err !== null){
+        reject({err})
+      }
+      if(JSON.stringify({fields}.fields) !== '{}' && JSON.stringify({files}.files) !== '{}'){
+        // exist files and fields
+        resolve({files,fields})
+      }
+      if(JSON.stringify({files}.files) !== '{}'){
+        // exist files but not exist fields
+        resolve({files})
+      }
+      if(JSON.stringify({fields}.fields) !== '{}'){
+        // exist fields but not exist files
+        resolve({fields})
+      }
+      
+    })
+  })
+}
+
+//main page
 let p = path.join(__dirname, "public");
 app.use(express.static(p));
 
+//session
+app.use(
+  expressSession({
+    //key to be encryted and exchange with client
+    secret: "You are who you are",
+    //will the session be renewed every time client and server build connection?
+    resave: true,
+    //need to build session for the first connection
+    saveUninitialized: true,
+    cookie: { maxAge: 30000 },
+  })
+);
+
+declare module "express-session" {
+  interface SessionData {
+    userId?: number;
+    count?: number;
+    isLogin?: boolean;
+  }
+}
+
 
 //login
+app.get ("/login", (req: Request, res: Response)=>{
+  res.sendFile(path.join(p,"login.html"));
+})
+
+//Local login
+app.post("/login", async(req:Request, res:Response)=>{
+  let result = {
+    isLogin:false,
+    errMess:'',
+    isError:false,
+    user:{username:''}
+  }
+
+  let formidable_result:any = await formidable_promise(req)
+  console.log(formidable_result);
+  const loginResult = await client.query(
+    `SELECT * FROM users WHERE username = $1`,
+      [formidable_result.fields.email]
+  );
+
+  if (loginResult.rowCount === 0) {
+    result.isLogin = false;
+    result.isError = true;
+    result.errMess = "Oops! User not found!";
+    res.json(result);
+  } else {
+    try{
+      // const match = await checkPassword (formidable_result.fields.password, loginResult.rows[0].password)
+      // if (match) {
+      if (loginResult.rows[0].password === formidable_result.fields.password) {
+        //for js
+        result.isLogin = true;
+        result.isError = false;
+        result.user.username = formidable_result.fields.username;
+        //for session
+        req.session.userId = loginResult.rows[0].id
+        req.session.isLogin = true;
+        //for response
+        res.json(result);
+      } else {
+        result.isLogin = false;
+        result.isError = true;
+        result.errMess = "Incorrect Password";
+        res.json(result);
+      }
+    } catch (err) {
+      console.log(err);
+      result.isLogin = false;
+      result.isError = true;
+      result.errMess = "Server error 500";
+      res.json(result);
+    }
+    }
+  });
 
 
 //logout
 
 
 //signup
+app.get ("signup", (req: Request, res: Response)=>{
+  res.sendFile(path.join(p,"signup.html"));
+})
 
 
 
---
+++ public/login.js
@@ -0,0 +1,20 @@
+//Login
+let loginForm = document.querySelector(".loginPanel");
+loginForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  formData = new FormData(loginForm);
+  //send request to server
+  let res = await fetch("/login", {
+    method: "POST",
+    body: formData,
+  });
+  let res_json = await res.json();
+  console.log(res_json);
+  if (res_json.isLogin) {
+    location.href = "./index.html";
+  } else {
+    alert(`${res_json.errMess}`);
+  }
+  loginForm.reset();
+});
+
