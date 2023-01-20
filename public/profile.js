8db05110171365f8ed5976519c70a1ef313b7199
hinwong1021@gmail.com <hinwong1021@gmail.com>
2023-01-20 19:15:08 +0800

    add post function active and hidden

--- public/profile.js
+++ public/profile.js
@@ -1,1 +1,9 @@
-// const post = document.querySelectora(".");
+const addNewPost = document.querySelector(".addpost");
+addNewPost.addEventListener("click", function () {
+  document.querySelector(".generate-post-container").classList.remove("hidden");
+});
+
+const cancel = document.querySelector(".cancel");
+cancel.addEventListener("click", function () {
+  document.querySelector(".generate-post-container").classList.add("hidden");
+});
